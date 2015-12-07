9e13e5b3e8cc713ef55d4cf033a5b298b20a3348
pareshsalunke <pareshsalunke@gmail.com>
2015-12-07 05:00:55 -0500

    Introduction to Controller- Simple Adding Machine

---
+++ js/addingmachine.js
@@ -0,0 +1,13 @@
+var app = angular.module("myApp", []);
+
+app.controller("FirstController", function($scope) {
+
+    $scope.counter = 0;
+    $scope.add = function(amount){
+            $scope.counter += amount;
+    };
+
+    $scope.subtract = function(amount) {
+        $scope.counter -= amount;
+    };
+});
